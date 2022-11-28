298a2bd7584a17168f77f952a82da292dd3bd165
Steve Manuel <steve@dylib.so>
2022-11-27 17:57:09 -0700

    feat: implement validation for imports & namespace

--- modsurfer-cli/src/cli/validate.rs
+++ modsurfer-cli/src/cli/validate.rs
@@ -2,18 +2,32 @@ use std::{collections::BTreeMap, path::PathBuf};
 
 use anyhow::Result;
 use modsurfer;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use tokio::io::AsyncReadExt;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize)]
 struct Validation {
     pub validate: Check,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize)]
 struct Check {
     pub url: Option<String>,
     pub wasi: Option<bool>,
+    pub imports: Option<Imports>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Namespace {
+    pub contains: Option<Vec<String>>,
+    pub excludes: Option<Vec<String>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Imports {
+    pub contains: Option<Vec<String>>,
+    pub excludes: Option<Vec<String>>,
+    pub namespace: Option<Namespace>,
 }
 
 #[derive(Debug)]
@@ -47,7 +61,6 @@ pub async fn validate_module(file: &PathBuf, check: &PathBuf) -> Result<()> {
         .await?;
 
     let validation: Validation = serde_yaml::from_slice(&buf)?;
-    println!("check: {:#?}", validation);
 
     let mut report = Report::new();
 
@@ -58,6 +71,59 @@ pub async fn validate_module(file: &PathBuf, check: &PathBuf) -> Result<()> {
             .contains(&"wasi_snapshot_preview1")
         {
             report.validate("wasi", wasi, true)
+        }
+    }
+
+    // Imports
+    if let Some(imports) = validation.validate.imports {
+        let import_func_names = module
+            .imports
+            .iter()
+            .map(|im| im.func.name.clone())
+            .collect::<Vec<_>>();
+
+        let import_module_names = module.get_import_namespaces();
+
+        if let Some(contains) = imports.contains {
+            contains.iter().for_each(|name| {
+                report.validate(
+                    &format!("imports.contains.{}", name),
+                    true,
+                    import_func_names.contains(name),
+                )
+            })
+        }
+
+        if let Some(excludes) = imports.excludes {
+            excludes.iter().for_each(|name| {
+                report.validate(
+                    &format!("imports.excludes.{}", name),
+                    false,
+                    import_func_names.contains(name),
+                )
+            })
+        }
+
+        if let Some(namespace) = imports.namespace {
+            if let Some(contains) = namespace.contains {
+                contains.iter().for_each(|name| {
+                    report.validate(
+                        &format!("imports.namespace.contains.{}", name),
+                        true,
+                        import_module_names.contains(&name.as_str()),
+                    );
+                })
+            }
+
+            if let Some(excludes) = namespace.excludes {
+                excludes.iter().for_each(|name| {
+                    report.validate(
+                        &format!("imports.namespace.excludes.{}", name),
+                        false,
+                        import_module_names.contains(&name.as_str()),
+                    )
+                })
+            }
         }
     }
 
