030cca5c27c4c64d836aad6dc857a6354fb2b029
Steve Manuel <steve@dylib.so>
2023-02-17 23:51:38 -0700

    feat: add docs link in comment of generated checkfiles

--- cli/src/cmd/generate.rs
+++ cli/src/cmd/generate.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
 
 use anyhow::Result;
@@ -16,8 +17,12 @@ pub async fn checkfile_from_module(wasm: &PathBuf, output: &PathBuf) -> Result<(
     let module = ModuleParser::parse(&module_data)?;
 
     let validation = generate_checkfile(&module).await?;
-    let file = File::create(output)?;
-    serde_yaml::to_writer(file, &validation)?;
+    let mut file = File::create(output)?;
+    writeln!(
+        &mut file,
+        "# For more information about other checkfile options, see the documentation at https://dev.dylib.so/docs/modsurfer/cli#checkfile"
+    )?;
+    serde_yaml::to_writer(&file, &validation)?;
 
     Ok(())
 }
